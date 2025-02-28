ba3c83994ce5435982735e4f07ac27df9be1cec8
bee <3172736837@qq.com>
2025-02-28 21:09:20 +0800

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/2/28.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
